7ba209008e1003fecb03ac149436ccbfa10dbac2
zafer.kirik <zafer.krk@hotmail.com>
2020-08-18 17:13:51 +0300

    xx

--- ExampleDBConnectionRN2/App.js
+++ ExampleDBConnectionRN2/App.js
@@ -7,17 +7,16 @@ export default class App extends Component {
   componentDidMount(){
     try {
       const config = {
-        server: '192.168.1.87', //ip address of the mssql database
+        server: '192.168.43.14', //ip address of the mssql database
         username: 'zaferkrk2', //username to login to the database
         password: '1234', //password to login to the database
         database: 'Northwind', //the name of the database to connect to
-        port: 1433, //OPTIONAL, port of the database on the server
+        //port: 1433, //OPTIONAL, port of the database on the server
       }
       MSSQL.connect(config);
-      const query = 'SELECT TOP * FROM Roles'
-      MSSQL.executeQuery(query);
-  
-      MSSQL.close();
+    // const query = 'SELECT TOP * FROM USERS'
+    // var x=MSSQL.executeQuery(query);
+    console.log(x);
     } catch (error) {
       console.log("ss",error);
     }
