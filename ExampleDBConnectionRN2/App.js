3d1efb9427e7eda2b4882d0e3c78ee44fe6c6bc0
zafer.kirik <zafer.krk@hotmail.com>
2020-08-19 13:10:14 +0300

    Fix MSSQL Connection Problem

--- ExampleDBConnectionRN2/App.js
+++ ExampleDBConnectionRN2/App.js
@@ -4,19 +4,26 @@ import MSSQL from 'react-native-mssql';
 
 export default class App extends Component {
 
-  componentDidMount(){
+  async componentDidMount(){
     try {
       const config = {
-        server: '192.168.43.14', //ip address of the mssql database
+        server: '192.168.70.102', //ip address of the mssql database
         username: 'zaferkrk2', //username to login to the database
         password: '1234', //password to login to the database
         database: 'Northwind', //the name of the database to connect to
         //port: 1433, //OPTIONAL, port of the database on the server
       }
-      MSSQL.connect(config);
-    // const query = 'SELECT TOP * FROM USERS'
-    // var x=MSSQL.executeQuery(query);
-    console.log(x);
+
+      await MSSQL.connect(config);
+      const query = 'SELECT * FROM Customers'
+       let rows=await MSSQL.executeQuery(query)
+       const x = "insert into Employees (LastName,FirstName) values ('esit','ss')"
+       MSSQL.executeQuery(x)
+       console.log("Customers",rows);
+       for(let item of rows){
+          console.log(item.City);
+       }
+    //console.log(x);
     } catch (error) {
       console.log("ss",error);
     }
